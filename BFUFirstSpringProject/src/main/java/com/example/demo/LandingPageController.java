404c290b55131dbdd15234e36fa4e5813626c08a
andreybozhkov <bozhkov.andrey@gmail.com>
2020-11-08 15:04:40 +0200

    Added first landing page controller

---
+++ BFUFirstSpringProject/src/main/java/com/example/demo/LandingPageController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LandingPageController {
+	@RequestMapping("/")
+	public String landingPage() {
+		return "landingPage";
+	}
+}
